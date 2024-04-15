b04afaf53765174b75c9259c50cb05609a70db6e
soeguet <osman_s@live.de>
2024-04-15 17:15:27 +0200

    init - vite, react, typescript, tailwindcss, eslint, prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "all", tabWidth: 4, semi: true, singleQuote: false,
+};
+
+module.exports = config;
