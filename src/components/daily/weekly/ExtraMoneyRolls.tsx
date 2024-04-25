70dcb003fb3baeec749baea3c233493cfccce34c
soeguet <osman_s@live.de>
2024-04-25 22:38:28 +0200

    add: support for extra rolls (WIP)

--- src/components/daily/weekly/ExtraMoneyRolls.tsx
+++ src/components/daily/weekly/ExtraMoneyRolls.tsx
@@ -3,7 +3,93 @@ function ExtraMoneyRolls() {
         <>
             <div className="bg-cyan-300 text-nowrap">
                 <h2>Extra Money Rolls</h2>
-                <p>Extra Money Rolls content</p>
+
+                <div className="grid grid-cols-3 pr-3 border border-red-700">
+                    <div className="bg-cyan-300 flex flex-col">
+                        <div className="border border-transparent m-2">2 €</div>
+                        <div className="border border-transparent m-2">1 €</div>
+                        <div className="border border-transparent m-2">
+                            50 c
+                        </div>
+                        <div className="border border-transparent m-2">
+                            20 c
+                        </div>
+                        <div className="border border-transparent m-2">
+                            10 c
+                        </div>
+                        <div className="border border-transparent m-2">5 c</div>
+                        <div className="border border-transparent m-2">2 c</div>
+                        <div className="border border-transparent m-2">1 c</div>
+                    </div>
+                    <div className="flex flex-col">
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                        <input
+                            type="number"
+                            className=" border border-black m-2 "
+                        />
+                    </div>
+                </div>
             </div>
         </>
     );
